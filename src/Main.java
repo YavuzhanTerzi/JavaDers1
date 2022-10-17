056080a9ddf27a689bde612942d24cc8093aa780
Yavuzhan Terzi <yavuzhanterzi@gmail.com>
2022-10-17 04:49:29 +0300

    ödev1-9

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        //------------------------------------------
+        int ogrenciSayisi = 12 ;
+        String mesaj = "Ögrenci sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Ögrenci sayim : " + ogrenciSayisi);
+        //-------------------------------------------------------
+        double sayi = 12.5;
+        sayi = -129 ;
+        char karakter = 'A';
+                boolean dogruMu=false;
+        //-----------------------------------------------------
+        int sayi1 = 24;
+        if (sayi1<20){
+            System.out.println("Sayi 20'den kücüktür");
+        } else if (sayi1==20) {
+            System.out.println("Sayi 20'ye esittir");
+        }
+        else {
+            System.out.println("Sayi 20'den büyüktür");
+        }
+        //----------------------------------------------
+
+    }
+    }
